0c5e1f5cd2e7b94037d87e65d0a6b7d5def31977
oeah2 <49606652+oeah2@users.noreply.github.com>
2021-10-19 16:31:55 +0200

    Updated example

--- src/main.c
+++ src/main.c
@@ -5,18 +5,21 @@
 #include <assert.h>
 #include "socket.h"
 
+/* This code demonstrates how to use this library */
+
 int main(void) {
-	puts("\n\nStart of SimpleHTTPGet Test: \n\n");
+	puts("Start of SimpleHTTPGet Test: \n");
 	char const* host = "www.google.de";
 	char const* file = "/";
 	char const* add_info = 0;
 	
 	char* http_response = http_get(host, file, add_info);
-	assert(http_response);
-	size_t resp_len = strlen(http_response);
-	printf("HTTP Response length: %zu\n", resp_len);
-	free(http_response);
-	
-	puts("\n\nEnd of SimpleHTTPGet Test!\n\n");
+	if(http_response) {
+		puts("HTTP get success!");
+		size_t response_length = strlen(http_response);
+		printf("HTTP Response length: %zu\n", response_length);
+		free(http_response);
+	} else
+		puts("Error during http_get.");
 	return 0;
 }
