348404ad0326fa72f6e4b0354631a5aec09d0868
oeah2 <49606652+oeah2@users.noreply.github.com>
2021-10-22 11:53:24 +0200

    Refactor

--- src/main.c
+++ src/main.c
@@ -7,27 +7,18 @@
 
 /* This code demonstrates how to use this library */
 
-int main(void) {
-	puts("Start of SimpleHTTPGet Test: \n");
-	char const* host = "www.google.com"; 	/* Host-Server */
-	char const* file = "/"; 				/* Requested file */
-	char const* add_info = 0; 				/* Additional information to be passed in the HTTP request */
-	
-	bool first_run = true;
-	struct HttpData http_response;
-GET:
-	http_response = http_get(host, file, add_info);
-	puts("HTTP get success!");
+void get_and_print(char const*const host, char const*const file, char const*const additional_info) {
+	struct HttpData http_response = http_get(host, file, additional_info);
 	size_t response_length = strlen(http_response.data);
 	printf("HTTP Response Code: %d\tHTTP Response length: %zu\t HTTP Received data bytes: %zu\t Received bytes: %zu\n", http_response.http_code, http_response.content_length, response_length, http_response.received_bytes);
 	if(http_response.data) free(http_response.data);
-	
-	host = "www.gogle.de";
-	if(first_run) {
-		first_run = false;
-		goto GET;
-	}
+}
 
-	puts("Error during http_get.");
+int main(void) {
+	puts("Start of SimpleHTTPGet Test: \n");
+	
+	get_and_print("www.google.com", "/", 0);
+	get_and_print("www.gogle.com", "/", 0);
+
 	return 0;
 }
