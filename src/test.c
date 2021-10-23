df2e0dafa5c4416f2cb5471bcb82bc8722e65520
Ahmet Ö <ozturk.ahmet@web.de>
2021-10-23 08:26:51 +0200

    Change testpages

--- src/test.c
+++ src/test.c
@@ -3,18 +3,18 @@
 
 int main(void) {
 	puts("\n\nStart of SimpleHTTPGet Test: \n\n");
-	char const* host = "www.google.com";
-	char const* file = "/";
+	char const* host = "www.columbia.edu";
+	char const* file = "/~fdc/sample.html";
 	char const* add_info = 0;
 	
 	char* http_response = http_get(host, file, add_info).data;
 	assert(http_response);
 	size_t resp_len = strlen(http_response);
-	printf("HTTP Response length: %zu\n", resp_len);
+	printf("HTTP Response length: %zu\n",  resp_len);
 	fflush(stdout);
 	free(http_response);
 	
-	http_response = http_get("www.gogle.com", file, 0).data;
+	http_response = http_get("www.gogle.com", "/", 0).data;
 	assert(http_response);
 	resp_len = strlen(http_response);
 	printf("HTTP Response length: %zu\n", resp_len);
